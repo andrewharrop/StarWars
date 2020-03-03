5aaeab041b9e404c5dff627bb50c93926e93dbd6
andrewharrop <60928763+andrewharrop@users.noreply.github.com>
2020-03-02 22:01:18 -0500

    Remade GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,39 +1,16 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.stage.Stage;
 
-import javax.swing.text.html.ImageView;
+
+import javafx.fxml.Initializable;
+
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    private Button submitButton;
-    @FXML
-    private RadioButton sithRadioButton;
-    @FXML
-    private RadioButton jediRadioButton;
-    @FXML
-    private RadioButton otherRadioButton;
-    @FXML
-    private TextField name;
-    @FXML
-    private TextArea extra;
-    @FXML
-    private Spinner offensive;
-    @FXML
-    private Spinner defensive;
-    @FXML
-    private Spinner health;
-    @FXML
-    private Spinner forceSkill;
-    @FXML
-    private ImageView starWarsLogo;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        Scene scene =new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
